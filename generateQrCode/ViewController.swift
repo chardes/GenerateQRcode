c4c4538e957505394b5ce421a77e8b6d1bfe09ec
Patrick CHARDES <patrick.chardes@gmail.com>
2018-06-13 21:41:35 +0200

    Initial Commit

---
+++ generateQrCode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  generateQrCode
+//
+//  Created by Patrick CHARDES on 13/06/2018.
+//  Copyright © 2018 Patrick CHARDES. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
