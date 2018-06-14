0e76991b7f939082aa97bd456c903dafbbbe37f2
Patrick CHARDES <patrick.chardes@gmail.com>
2018-06-14 16:58:36 +0200

    It's Ok

--- generateQrCode/ViewController.swift
+++ generateQrCode/ViewController.swift
@@ -10,16 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ui_dataField: UITextField!
+    @IBOutlet weak var ui_codeSelector: UISegmentedControl!
+    @IBOutlet weak var ui_displayCodeView: UIImageView!
+    
+    var filter:CIFilter!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func ui_generatePressed(_ sender: UIButton) {
+        if let text = ui_dataField.text {
+            let data = text.data(using: .ascii, allowLossyConversion: false)
+            
+            if ui_codeSelector.selectedSegmentIndex == 0 {
+                // Barcode
+                filter = CIFilter(name: "CICode128BarcodeGenerator")
+            } else {
+                // QRCode
+                filter = CIFilter(name: "CIQRCodeGenerator")
+            }
+            filter.setValue(data, forKey: "inputMessage")
+            
+            // 1er version qui fonctionne
+            // let image = UIImage(ciImage: filter.outputImage!)
+            
+            // 2eme version pour une image plus nette
+            let transform = CGAffineTransform(scaleX: 10, y: 10)
+            let image = UIImage(ciImage: filter.outputImage!.transformed(by: transform))
+            
+            
+            ui_displayCodeView.image = image
+        }
     }
-
 
 }
 
